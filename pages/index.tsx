327450cbff91e6545777f29a26c7636beb16058d
Phil <phil.nice.23@gmail.com>
2021-05-27 18:46:56 +0300

    add buttons

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,15 @@
+import React from 'react';
+import { Htag } from '../components';
+import { Button } from '../components';
+
 export default function Home(): JSX.Element {
-  return <div></div>;
+	return (
+		<div>
+			<Htag tag="h1">Текст</Htag>
+			<Button appearance="primary" className="asdasd">
+				Кнопка
+			</Button>
+			<Button appearance="ghost">Кнопка</Button>
+		</div>
+	);
 }
