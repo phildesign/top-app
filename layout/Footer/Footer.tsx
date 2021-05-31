6f8721908e145b705b393b1fac30a7ff6b31e0ec
Phil <phil.nice.23@gmail.com>
2021-06-01 00:48:20 +0300

    add styles

--- layout/Footer/Footer.tsx
+++ layout/Footer/Footer.tsx
@@ -1,6 +1,18 @@
 import { FooterProps } from './Footer.props';
 import styles from './Footer.module.css';
+import cn from 'classnames';
+import { format } from 'date-fns';
 
-export const Footer = ({ ...props }: FooterProps): JSX.Element => {
-	return <div {...props}>Footer</div>;
+export const Footer = ({ className, ...props }: FooterProps): JSX.Element => {
+	return (
+		<footer className={cn(className, styles.footer)} {...props}>
+			<div>OwlTop © 2020 - {format(new Date(), 'yyyy')} Все права защищены</div>
+			<a href="#" target="_blank" className={styles.link}>
+				Пользовательское соглашение
+			</a>
+			<a href="#" target="_blank" className={styles.link}>
+				Политика конфиденциальности
+			</a>
+		</footer>
+	);
 };
