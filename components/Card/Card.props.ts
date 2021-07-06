b2f722b81f8b2028fc31db88ed84d3613df55a02
Phil <phil.nice.23@gmail.com>
2021-07-06 15:41:16 +0300

    add Card and HhData

---
+++ components/Card/Card.props.ts
@@ -0,0 +1,7 @@
+import { DetailedHTMLProps, HtmlHTMLAttributes, ReactNode } from 'react';
+
+export interface CardProps
+	extends DetailedHTMLProps<HtmlHTMLAttributes<HTMLDivElement>, HTMLDivElement> {
+	color: 'white' | 'blue';
+	children: ReactNode;
+}
