b81bd22f2efa9ab26b6b9bc962eacc0a9d662aa5
Phil <phil.nice.23@gmail.com>
2021-07-08 01:40:47 +0300

    add Advantages and Inner Html

--- page-components/TopPageComponent/TopPageComponent.tsx
+++ page-components/TopPageComponent/TopPageComponent.tsx
@@ -1,4 +1,4 @@
-import { Card, HhData, Htag, Tag } from '../../components';
+import { Advantages, Card, HhData, Htag, Ptag, Tag } from '../../components';
 import { TopPageComponentProps } from './TopPageComponent.props';
 import styles from './TopPageComponent.module.css';
 import { TopLevelCategory } from '../../interfaces/page.interface';
@@ -26,7 +26,23 @@ export const TopPageComponent = ({
 					hh.ru
 				</Tag>
 			</div>
-			{firstCategory == TopLevelCategory.Courses && <HhData {...page.hh} />}
+			{firstCategory == TopLevelCategory.Courses && page.hh && <HhData {...page.hh} />}
+			{page.advantages && page.advantages.length > 0 && (
+				<>
+					<Htag tag="h2">Преимущества</Htag>
+					<Advantages advantages={page.advantages} />
+				</>
+			)}
+
+			{page.seoText && (
+				<div className={styles.seo} dangerouslySetInnerHTML={{ __html: page.seoText }} />
+			)}
+			<Htag tag="h2">Получаемые навыки</Htag>
+			{page.tags.map((t) => (
+				<Tag key={t} color="primary">
+					{t}
+				</Tag>
+			))}
 		</div>
 	);
 };
