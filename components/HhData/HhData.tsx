55f07bf1b3af67858fd9bfcb97f5457a15fbdf33
Phil <phil.nice.23@gmail.com>
2021-07-08 00:23:32 +0300

    add Regex

--- components/HhData/HhData.tsx
+++ components/HhData/HhData.tsx
@@ -4,6 +4,7 @@ import cn from 'classnames';
 import React from 'react';
 import RateIcon from './rate.svg';
 import { Card } from '../Card/Card';
+import { priceRu } from '../../helpers/helpers';
 
 export const HhData = ({
 	count,
@@ -20,7 +21,7 @@ export const HhData = ({
 			<Card className={styles.salary}>
 				<div>
 					<div className={styles.title}>Начальный</div>
-					<div className={styles.salaryValue}>{juniorSalary}</div>
+					<div className={styles.salaryValue}>{priceRu(juniorSalary)}</div>
 					<div className={styles.rate}>
 						<RateIcon className={styles.filled} />
 						<RateIcon />
@@ -29,7 +30,7 @@ export const HhData = ({
 				</div>
 				<div>
 					<div className={styles.title}>Средний</div>
-					<div className={styles.salaryValue}>{middleSalary}</div>
+					<div className={styles.salaryValue}>{priceRu(middleSalary)}</div>
 					<div className={styles.rate}>
 						<RateIcon className={styles.filled} />
 						<RateIcon className={styles.filled} />
@@ -38,7 +39,7 @@ export const HhData = ({
 				</div>
 				<div>
 					<div className={styles.title}>Профессионал</div>
-					<div className={styles.salaryValue}>{seniorSalary}</div>
+					<div className={styles.salaryValue}>{priceRu(seniorSalary)}</div>
 					<div className={styles.rate}>
 						<RateIcon className={styles.filled} />
 						<RateIcon className={styles.filled} />
