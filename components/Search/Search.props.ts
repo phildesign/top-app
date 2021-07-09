7cb7d89bc8cdb425f66d432dc9ecc39b6a19eb91
Phil <phil.nice.23@gmail.com>
2021-07-09 22:05:11 +0300

    add Search

---
+++ components/Search/Search.props.ts
@@ -0,0 +1,4 @@
+import { DetailedHTMLProps, HtmlHTMLAttributes, ReactNode } from 'react';
+
+export interface SearchProps
+	extends DetailedHTMLProps<HtmlHTMLAttributes<HTMLDivElement>, HTMLDivElement> {}
