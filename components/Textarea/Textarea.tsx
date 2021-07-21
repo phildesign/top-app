99c207493356ad640e1d2c1a3933c8d1c44c079f
Phil <phil.nice.23@gmail.com>
2021-07-21 23:47:53 +0300

    add error handling for Input and Textarea

--- components/Textarea/Textarea.tsx
+++ components/Textarea/Textarea.tsx
@@ -4,7 +4,21 @@ import cn from 'classnames';
 import { ForwardedRef, forwardRef } from 'react';
 
 export const Textarea = forwardRef(
-	({ className, ...props }: TextareaProps, ref: ForwardedRef<HTMLTextAreaElement>): JSX.Element => {
-		return <textarea className={cn(className, styles.input)} ref={ref} {...props} />;
+	(
+		{ className, error, ...props }: TextareaProps,
+		ref: ForwardedRef<HTMLTextAreaElement>,
+	): JSX.Element => {
+		return (
+			<div className={cn(styles.textareaWrapper, className)}>
+				<textarea
+					className={cn(styles.textarea, {
+						[styles.error]: error,
+					})}
+					ref={ref}
+					{...props}
+				/>
+				{error && <span className={styles.errorMessage}>{error.message}</span>}
+			</div>
+		);
 	},
 );
