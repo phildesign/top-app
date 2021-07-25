c059bbd771785aa058b6c33a2c07716e8a7bad2a
Phil <phil.nice.23@gmail.com>
2021-07-25 22:32:19 +0300

    add animation for Product

--- page-components/TopPageComponent/TopPageComponent.tsx
+++ page-components/TopPageComponent/TopPageComponent.tsx
@@ -35,7 +35,9 @@ export const TopPageComponent = ({
 				)}
 				<Sort sort={sort} setSort={setSort} />
 			</div>
-			<div>{sortedProducts && sortedProducts.map((p) => <Product key={p._id} product={p} />)}</div>
+			<div>
+				{sortedProducts && sortedProducts.map((p) => <Product layout key={p._id} product={p} />)}
+			</div>
 			<div className={styles.hhTitle}>
 				<Htag tag="h2">Вакансии - {page.category}</Htag>
 				<Tag color="red" size="m">
