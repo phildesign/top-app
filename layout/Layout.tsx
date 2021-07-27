a3076356388e22e9b658a51ab82671aee953e4e5
Phil <phil.nice.23@gmail.com>
2021-07-27 23:14:59 +0300

    add Up

--- layout/Layout.tsx
+++ layout/Layout.tsx
@@ -5,6 +5,7 @@ import { Header } from './Header/Header';
 import { Sidebar } from './Sidebar/Sidebar';
 import { Footer } from './Footer/Footer';
 import { AppContextProvider, IAppContext } from '../context/app.context';
+import { Up } from '../components';
 
 const Layout = ({ children }: LayoutProps): JSX.Element => {
 	return (
@@ -13,6 +14,7 @@ const Layout = ({ children }: LayoutProps): JSX.Element => {
 			<Sidebar className={styles.sidebar} />
 			<div className={styles.body}>{children}</div>
 			<Footer className={styles.footer} />
+			<Up />
 		</div>
 	);
 };
