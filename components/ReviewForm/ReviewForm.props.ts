b35ae9069c724ed900fdff3fc1b389c1d2927949
Phil <phil.nice.23@gmail.com>
2021-08-02 23:00:41 +0300

    add accessibility for sorting

--- components/ReviewForm/ReviewForm.props.ts
+++ components/ReviewForm/ReviewForm.props.ts
@@ -1,4 +1,7 @@
-export interface ReviewFormProps {
+import { DetailedHTMLProps, HTMLAttributes } from 'react';
+
+export interface ReviewFormProps
+	extends DetailedHTMLProps<HTMLAttributes<HTMLDivElement>, HTMLDivElement> {
 	productId: string;
 	isOpened: boolean;
 }
--- components/Sort/Sort.tsx
+++ components/Sort/Sort.tsx
@@ -6,20 +6,20 @@ import SortIcon from './sort.svg';
 export const Sort = ({ sort, setSort, className, ...props }: SortProps): JSX.Element => {
 	return (
 		<div className={cn(styles.sort, className)} {...props}>
-			<span
+			<button
 				onClick={() => setSort(SortEnum.Rating)}
 				className={cn({
 					[styles.active]: sort == SortEnum.Rating,
 				})}>
 				<SortIcon className={styles.sortIcon} /> По рейтингу
-			</span>
-			<span
+			</button>
+			<button
 				onClick={() => setSort(SortEnum.Price)}
 				className={cn({
 					[styles.active]: sort == SortEnum.Price,
 				})}>
 				<SortIcon className={styles.sortIcon} /> По цене
-			</span>
+			</button>
 		</div>
 	);
 };
