e5e3ae2c787db339ce59195cf1a3dae8b42eaafd
Phil <phil.nice.23@gmail.com>
2021-08-04 20:46:57 +0300

    add landmarks

--- components/Search/Search.tsx
+++ components/Search/Search.tsx
@@ -27,7 +27,7 @@ export const Search = ({ className, ...props }: SearchProps): JSX.Element => {
 	};
 
 	return (
-		<div className={cn(className, styles.search)} {...props}>
+		<form className={cn(className, styles.search)} {...props} role="search">
 			<Input
 				className={styles.input}
 				placeholder="Поиск..."
@@ -42,6 +42,6 @@ export const Search = ({ className, ...props }: SearchProps): JSX.Element => {
 				aria-label="Искать по сайту">
 				<GlassIcon />
 			</Button>
-		</div>
+		</form>
 	);
 };
