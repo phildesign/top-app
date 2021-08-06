1ffa84a70818bdf0f259c45c7f9dcd6903125767
Phil <phil.nice.23@gmail.com>
2021-08-06 23:30:41 +0300

    add accessibility for notification

--- components/ReviewForm/ReviewForm.tsx
+++ components/ReviewForm/ReviewForm.tsx
@@ -103,16 +103,26 @@ export const ReviewForm = ({
 				</div>
 			</div>
 			{isSuccess && (
-				<div className={cn(styles.success, styles.panel)}>
+				<div className={cn(styles.success, styles.panel)} role="alert">
 					<div className={styles.successTitle}>Ваш отзыв отправлен</div>
 					<div>Спасибо, ваш отзыв будет опубликован после проверки.</div>
-					<CloseIcon className={styles.close} onClick={() => setIsSuccess(false)} />
+					<button
+						onClick={() => setIsSuccess(false)}
+						className={styles.close}
+						aria-label="Закрыть оповещение">
+						<CloseIcon />
+					</button>
 				</div>
 			)}
 			{error && (
 				<div className={cn(styles.error, styles.panel)}>
 					Что-то пошло не так, попробуйте обновить страницу
-					<CloseIcon className={styles.close} onClick={() => setError(undefined)} />
+					<button
+						onClick={() => setError(undefined)}
+						className={styles.close}
+						aria-label="Закрыть оповещение">
+						<CloseIcon />
+					</button>
 				</div>
 			)}
 		</form>
