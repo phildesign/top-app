9862734c243e9bc0f12b5af4e4f1894f8029ef69
Phil <phil.nice.23@gmail.com>
2021-08-09 23:25:13 +0300

    add useReducedMotion

--- layout/Menu/Menu.tsx
+++ layout/Menu/Menu.tsx
@@ -6,20 +6,23 @@ import { FirstLevelMenuItem, PageItem } from '../../interfaces/menu.interface';
 import Link from 'next/link';
 import { useRouter } from 'next/router';
 import { firstLevelMenu } from '../../helpers/helpers';
-import { motion } from 'framer-motion';
+import { motion, useReducedMotion } from 'framer-motion';
 
 export const Menu = (): JSX.Element => {
 	const { menu, setMenu, firstCategory } = useContext(AppContext);
 	const [announce, setAnnounce] = useState<'closed' | 'opened' | undefined>();
+	const shouldReduceMotion = useReducedMotion();
 	const router = useRouter();
 
 	const variants = {
 		visible: {
 			marginBottom: 20,
-			transition: {
-				when: 'beforeChildren',
-				staggerChildren: 0.1,
-			},
+			transition: shouldReduceMotion
+				? {}
+				: {
+						when: 'beforeChildren',
+						staggerChildren: 0.1,
+				  },
 		},
 		hidden: {
 			marginBottom: 0,
@@ -32,7 +35,7 @@ export const Menu = (): JSX.Element => {
 			height: 29,
 		},
 		hidden: {
-			opacity: 0,
+			opacity: shouldReduceMotion ? 1 : 0,
 			height: 0,
 		},
 	};
--- page-components/TopPageComponent/TopPageComponent.tsx
+++ page-components/TopPageComponent/TopPageComponent.tsx
@@ -5,6 +5,7 @@ import { TopLevelCategory } from '../../interfaces/page.interface';
 import { SortEnum } from '../../components/Sort/Sort.props';
 import { useEffect, useReducer } from 'react';
 import { sortReducer } from './sort.reducer';
+import { useReducedMotion } from 'framer-motion';
 
 export const TopPageComponent = ({
 	page,
@@ -15,6 +16,7 @@ export const TopPageComponent = ({
 		products,
 		sort: SortEnum.Rating,
 	});
+	const shouldReduceMotion = useReducedMotion();
 
 	const setSort = (sort: SortEnum) => {
 		dispatchSort({ type: sort });
@@ -37,7 +39,14 @@ export const TopPageComponent = ({
 			</div>
 			<div role="list">
 				{sortedProducts &&
-					sortedProducts.map((p) => <Product role="listitem" layout key={p._id} product={p} />)}
+					sortedProducts.map((p) => (
+						<Product
+							role="listitem"
+							layout={shouldReduceMotion ? false : true}
+							key={p._id}
+							product={p}
+						/>
+					))}
 			</div>
 			<div className={styles.hhTitle}>
 				<Htag tag="h2">Вакансии - {page.category}</Htag>
